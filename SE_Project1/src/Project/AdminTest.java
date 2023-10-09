c999579273e7d4885a527f3ed7ff6f3f7646919b
John James <21389jwj@gmail.com>
2023-10-09 13:58:33 -0400

    re-added test classes

---
+++ SE_Project1/src/Project/AdminTest.java
@@ -0,0 +1,15 @@
+package Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AdminTest {
+	
+	@Test
+	void testConstructor() {
+		Admin a = new Admin("Billy", "0010", "billyiscool");
+		
+	}
+
+}
--- SE_Project1/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module SE_Project1 {
-	requires javafx.controls;
-	requires org.junit.jupiter.api;
-	requires junit;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
