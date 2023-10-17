3ecbb7f5d2e3484ba0b1cb5f0fd87d6b70474f49
John James <21389jwj@gmail.com>
2023-10-17 16:51:43 -0400

    added joinGroup and leaveGroup test

--- SE_Project/src/Project/UserTest.java
+++ SE_Project/src/Project/UserTest.java
@@ -1,6 +1,9 @@
 package Project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
@@ -28,5 +31,31 @@ public class UserTest {
 		
 		assertEquals(true, actual);
 	}
+	
+	@Test
+	void testjoinGroup_Success() {
+		User u = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
+		Group g = new Group("fun");
+		ArrayList<Group> group = new ArrayList<>();
+		group.add(g);
+
+		Boolean actual = u.joinGroup("Jack", group, u);
+		
+		assertEquals(true, actual);
+	}
+	
+	@Test
+	void testjoinGroup_Fail() {
+		User u = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
+		Group g = new Group("fun");
+		g.addMember(u);
+		ArrayList<Group> group = new ArrayList<>();
+		group.add(g);
+
+		Boolean actual = u.joinGroup("Jack", group, u);
+		
+		assertEquals(false, actual);
+	}
+	
 
 }
