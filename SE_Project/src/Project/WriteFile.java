f3549e0a21b3f8a2113bdde201377977fc4d8442
Ryan Scandrol <rkscandrol@gmail.com>
2023-10-25 20:09:32 -0400

    Fixed syntax in UserTest class that was causing errors. Added WriteFile class and methods to write users to a text file and appropriate tests.

---
+++ SE_Project/src/Project/WriteFile.java
@@ -0,0 +1,47 @@
+package Project;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WriteFile {
+	
+	
+	public static void writeFile(SystemManager manager, String fileName) throws IOException {
+		
+		try {
+			
+			File dataFile = new File(fileName);
+			
+			if (!dataFile.exists()) {
+				dataFile.createNewFile();
+			}
+			
+			FileWriter writer = new FileWriter(fileName);
+			
+			writeUsers(manager, writer);
+			
+			writer.close();
+			
+		} catch (IOException e) {
+			throw new IOException();
+		}
+		
+	}
+	
+	
+	
+	private static void writeUsers(SystemManager manager, FileWriter writer) throws IOException {
+		
+		ArrayList<User> users = manager.getUsers_Alphabetically();
+		
+		for (User u : users) {
+			String userData = u.getUserWriteData();
+			
+			writer.write(userData);
+		}
+		
+	}
+
+}
---
+++ SE_Project/src/Project/WriteFileTest.java
@@ -0,0 +1,44 @@
+package Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class WriteFileTest {
+
+	@Test
+	void testWriteFile_Users() {
+		
+		User u1 = new User("Jack", "jackster3", "HKb@wser!", "10/10/1997", "Valdosta", "Georgia");
+		User u2 = new User("Dan", "theWiz", "WartH@g77", "10/10/1997", "Valdosta", "Georgia");
+		User u3 = new User("Carol", "WestCarolina", "P!zzaH$t", "10/10/1997", "Valdosta", "Georgia");
+		User u4 = new User("Dulaney", "LegalTrouble", "D@uble&Tr@uble", "10/10/1997", "Valdosta", "Georgia");
+		User u5 = new User("Ethan", "IDK", "WHY#5", "10/10/1997", "Valdosta", "Georgia");
+		
+		SystemManager manager = new SystemManager();
+		
+		manager.addUser(u1);
+		manager.addUser(u2);
+		manager.addUser(u3);
+		manager.addUser(u4);
+		manager.addUser(u5);
+		
+		String fileName = "C:\\Users\\rks11\\git\\23fa_team2\\SE_Project\\src\\Project\\WriteFile_Test_Users.txt";
+		
+		try {
+			
+			WriteFile.writeFile(manager, fileName);
+			
+			assertEquals(true, true);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+	}
+
+}
