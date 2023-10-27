977b9ae2053daf008479fdaa2f40ce133f734cb6
Ryan Scandrol <rkscandrol@gmail.com>
2023-10-26 21:05:05 -0400

    Moved Text Files to there own folder and updated file paths

--- SE_Project/src/Project/WriteFileTest.java
+++ SE_Project/src/Project/WriteFileTest.java
@@ -25,7 +25,7 @@ class WriteFileTest {
 		manager.addUser(u4);
 		manager.addUser(u5);
 		
-		String fileName = ".\\SE_Project\\src\\Project\\WriteFile_Test_Users.txt";
+		String fileName = ".\\SE_Project\\src\\Project\\TextFiles\\WriteFile_Test_Users.txt";
 		
 		try {
 			
