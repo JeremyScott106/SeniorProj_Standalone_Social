e9bf9b0bc3a863a2c78816bcdbfb52b46b26dbd8
John James <21389jwj@gmail.com>
2023-11-01 13:29:31 -0400

    updated tests

--- SE_Project/src/Project/ResponseTest.java
+++ SE_Project/src/Project/ResponseTest.java
@@ -1,20 +1,43 @@
 package Project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class ResponseTest {
 	
 	//Tests getting the ID of the parent post.
 	@Test
-	void getPaternalIDTest() {
+	void getUser() {
 		Group testGroup1 = new Group("Standard Name");
-		User testUser1 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
 		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
-		Post testPost1 = new Post (testUser1, testGroup1, 1, "I'm posting.");
-		Response testResponse1 = new Response(testUser2, testGroup1, 2, "I disagree.", testPost1);
+		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
 		
-		assertEquals(1, testResponse1.getPaternalPostID());
+		assertEquals(testUser2, testResponse1.getUser());
+	}
+	
+	//Tests getting the ID of the parent post.
+	@Test
+	void getGroup() {
+		Group testGroup1 = new Group("Standard Name");
+		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
+		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
+		
+		assertEquals(testGroup1, testResponse1.getGroup());
+	}
+	
+	
+	
+	//Tests getting the ID of the parent post.
+	@Test
+	void getResponseBody() {
+		Group testGroup1 = new Group("Standard Name");
+		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
+		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
+		
+		assertEquals("I disagree.", testResponse1.getResponceBody());
 	}
 
 }
