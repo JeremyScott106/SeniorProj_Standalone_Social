293929988865eac230112a3b51d9aa20c32fdba0
John James <21389jwj@gmail.com>
2023-11-01 21:06:04 -0400

    minor changes

--- SE_Project/src/Project/Response.java
+++ SE_Project/src/Project/Response.java
@@ -3,12 +3,12 @@ package Project;
 public class Response{
 	private User user;
 	private Group group;
-	private String responceBody;
+	private String responseBody;
 	
-	public Response(User user, Group group, String responceBody) {
+	public Response(User user, Group group, String responseBody) {
 		this.user = user;
 		this.group = group;
-		this.responceBody = responceBody;
+		this.responseBody = responseBody;
 	}
 	
 	public User getUser() {
@@ -19,8 +19,8 @@ public class Response{
 		return group;
 	}
 	
-	public String getResponceBody() {
-		return responceBody;
+	public String getResponseBody() {
+		return responseBody;
 	}
 	
 }
