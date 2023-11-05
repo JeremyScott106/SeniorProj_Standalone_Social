6bfeb40eb30a57756bdf01b8289fac7d68d03c86
John James <21389jwj@gmail.com>
2023-11-04 22:37:04 -0400

    updated add response method

--- SE_Project/src/Project/Post.java
+++ SE_Project/src/Project/Post.java
@@ -6,18 +6,16 @@ public class Post {
 	
 	private User user;
 	private Group group;
-	private int id;
 	private String postBody;
-	private ArrayList<Response> response;
+	private ArrayList<Response> responses;
     private int score;
     java.util.Date dateTime;
     
-    public Post (User user, Group group, int id, String postBody) {
-    	this.user = user;
-    	this.group = group;
-    	this.id = id;
+    public Post (membership memberships, String postBody) {
+    	this.user = memberships.getUser();
+    	this.group = memberships.getGroup();
     	this.postBody = postBody;
-    	this.response = new ArrayList<>();
+    	this.responses = new ArrayList<>();
     	this.dateTime = new java.util.Date(); //should save the current date and time.
     	this.score = 0;
     }
@@ -29,23 +27,18 @@ public class Post {
 	public Group getGroup() {
 		return group;
 	}
-	
-	public int getId() {
-		return id;
-	}
-	
+
 	public String getPostBody() {
 		return postBody;
 	}
 	
 	public ArrayList<Response> getResponse() {
-		return response;
+		return responses;
 	}
 	
     //Adds responses into responses
-    public void addResponse(User user, Group group, String postBody) {
-    	Response r1 = new Response(user, group, postBody);
-    	response.add(r1);
+    public void addResponse(Response r) {
+        responses.add(r);
     }
 	
 	public int getScore() {
