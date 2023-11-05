d44d0cd969cac8bac6ffa43041346799ae66311c
John James <21389jwj@gmail.com>
2023-11-04 22:36:46 -0400

    fixed test method

--- SE_Project/src/Project/ResponseTest.java
+++ SE_Project/src/Project/ResponseTest.java
@@ -8,34 +8,13 @@ import org.junit.jupiter.api.Test;
 
 public class ResponseTest {
 	
-	//Tests getting the ID of the parent post.
-	@Test
-	void getUser() {
-		Group testGroup1 = new Group("Standard Name");
-		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
-		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
-		
-		assertEquals(testUser2, testResponse1.getUser());
-	}
-	
-	//Tests getting the ID of the parent post.
-	@Test
-	void getGroup() {
-		Group testGroup1 = new Group("Standard Name");
-		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
-		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
-		
-		assertEquals(testGroup1, testResponse1.getGroup());
-	}
-	
-	
-	
-	//Tests getting the ID of the parent post.
+	//Tests getting the responseBody of the response object.
 	@Test
 	void getResponseBody() {
 		Group testGroup1 = new Group("Standard Name");
 		User testUser2 = new User("Bob", "ID", "pw", "11/11/2001", "Valdosta", "GA");
-		Response testResponse1 = new Response(testUser2, testGroup1, "I disagree.");
+		membership m = new membership(testUser2, testGroup1);
+		Response testResponse1 = new Response(m, "I disagree.");
 		
 		assertEquals("I disagree.", testResponse1.getResponseBody());
 	}
