63da93e2939933ab8f6c1b464b0b24e30ece8485
John James <21389jwj@gmail.com>
2023-11-04 22:38:48 -0400

    minor changes

--- SE_Project/src/Project/membership.java
+++ SE_Project/src/Project/membership.java
@@ -1,5 +1,8 @@
 package Project;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -7,6 +10,17 @@ public class membership {
 	private User user;
 	private Group group;
 	private Date registeredDate;
+	
+	public membership(User user, Group group, String registeredDate) {
+		this.user = user;
+		this.group = group;
+		try {
+			DateFormat df = new SimpleDateFormat("mm/dd/yyyy");
+			this.registeredDate = df.parse(registeredDate);
+		}catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public membership(User user, Group group) {
         this.user = user;
