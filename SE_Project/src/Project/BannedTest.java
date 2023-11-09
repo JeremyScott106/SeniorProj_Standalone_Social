ca7b03ab721ad9fcf768bfcf56b98c656978700e
John James <21389jwj@gmail.com>
2023-11-09 16:33:22 -0500

    fixed tests

--- SE_Project/src/Project/BannedTest.java
+++ SE_Project/src/Project/BannedTest.java
@@ -43,21 +43,7 @@ public class BannedTest {
 
 		Group actual = b.getGroup();
 		assertEquals(testGroup1, actual);
-	}	
-	
-	@Test
-	void testGetDate() {
-		User u = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
-		Group testGroup1 = new Group("Standard Name");
-		
-
-		// Create a Date object for the registration date.
-		Banned b = new Banned(u, testGroup1, "01/12/2012");
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-		String actual = df.format(b.getDate());
-		
-		assertEquals("01/12/2012", actual);
-	}	
+	}
 	
 	
 	@Test
@@ -65,7 +51,7 @@ public class BannedTest {
 		User u = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup1 = new Group("Standard Name");
 		
-		Banned b = new Banned(u, testGroup1, "01/12/2012");
+		Banned b = new Banned(u, testGroup1);
 				
 		String actual = b.getBannedWriteData();
 		
@@ -73,7 +59,6 @@ public class BannedTest {
 							"@BANNED\n" +
 							"@USER=jackster3\n" + 
 							"@GROUP=Standard Name\n" + 
-							"@REGISTEREDDATE=01/12/2012\n" + 
 							"@END\n\n";
 		
 		assertEquals(expected, actual);
@@ -86,12 +71,12 @@ public class BannedTest {
 		User u1 = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup1 = new Group("Standard Name");
 		
-		Banned b1 = new Banned(u1, testGroup1, "01/12/2012");
+		Banned b1 = new Banned(u1, testGroup1);
 		
 		User u2 = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup2 = new Group("Standard Name");
 		
-		Banned b2 = new Banned(u2, testGroup2, "01/12/2012");
+		Banned b2 = new Banned(u2, testGroup2);
 
 			
 		int actual = b1.compareTo(b2);
@@ -105,12 +90,12 @@ public class BannedTest {
 		User u1 = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup1 = new Group("Standard Name");
 		
-		Banned b1 = new Banned(u1, testGroup1, "01/12/2012");
+		Banned b1 = new Banned(u1, testGroup1);
 		
 		User u2 = new User("Jack", "test", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup2 = new Group("Standard Name");
 		
-		Banned b2 = new Banned(u2, testGroup2, "01/12/2012");
+		Banned b2 = new Banned(u2, testGroup2);
 		
 		int actual = b1.compareTo(b2);
 		
@@ -118,17 +103,17 @@ public class BannedTest {
 	}
 	
 	@Test
-	void testCompareTo_Failure_DifferentGruopNames() {
+	void testCompareTo_Failure_DifferentGroupNames() {
 		
 		User u1 = new User("Jack", "jackster3", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup1 = new Group("Standard Name");
 		
-		Banned b1 = new Banned(u1, testGroup1, "01/12/2012");
+		Banned b1 = new Banned(u1, testGroup1);
 		
 		User u2 = new User("Jack", "test", "HKb@wser!", "06/17/2000", "Valdosta", "Georgia", "12/17/2007");
 		Group testGroup2 = new Group("Test");
 		
-		Banned b2 = new Banned(u2, testGroup2, "01/12/2012");
+		Banned b2 = new Banned(u2, testGroup2);
 		
 		int actual = b1.compareTo(b2);
 		
