13041fad6b2258e27602f82cd3663212d64f15ed
John James <21389jwj@gmail.com>
2023-11-21 23:33:06 -0500

    added voted class

--- SE_Project/src/Project/Voted.java
+++ SE_Project/src/Project/Voted.java
@@ -7,6 +7,7 @@ public class Voted {
 	private User user;
 	private Post post;
 	
+	//test:1
 	public Voted (User u, Post p){
 		this.user = u;
 		this.post = p;
@@ -14,32 +15,39 @@ public class Voted {
 		hasDownvoted = false;
 	}
 	
+	//test:1
 	public void up() {
 		hasUpvoted = true;
 		hasDownvoted = false;
 	}
 	
+	//test:1
 	public void down() {
 		hasUpvoted = false;
 		hasDownvoted = true;
 	}
 	
+	//test:1
 	public boolean getUp(){
 		return hasUpvoted;
 	}
 	
+	//test:1
 	public boolean getDown(){
 		return hasDownvoted;
 	}
 	
+	//test:1
 	public User getUser(){
 		return user;
 	}
 	
+	//test:1
 	public Post getPost(){
 		return post;
 	}
 	
+	//test:3
 	public boolean compareTo(Voted v) {
 		if (v.getUser().compareId(user.getId())) {
 			if(v.getPost().compareTo(post) == 1) {
