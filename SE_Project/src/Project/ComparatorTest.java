869bfaaf6f24b017421bfc5b9b146991ac9d1bb8
John James <21389jwj@gmail.com>
2023-11-21 23:31:21 -0500

    added more tests

--- SE_Project/src/Project/ComparatorTest.java
+++ SE_Project/src/Project/ComparatorTest.java
@@ -11,7 +11,7 @@ class ComparatorTest {
 
 	
 	@Test
-	void testSortCategoriesByName() {
+	void testSortCategoriesByName_Success() {
 		
 		category c1 = new category("Sports");
 		category c2 = new category("Games");
@@ -45,9 +45,44 @@ class ComparatorTest {
 		
 	}
 	
+	@Test
+	void testSortCategoriesByName_Failure() {
+		
+		category c1 = new category("Sports");
+		category c2 = new category("Games");
+		category c3 = new category("Video Games");
+		category c4 = new category("Foods");
+		category c5 = new category("fail");
+		
+		ArrayList<category> actual = new ArrayList<category>();
+		
+		actual.add(c1);
+		actual.add(c2);
+		actual.add(c3);
+		actual.add(c4);
+		actual.add(c5);
+		
+		String expected[] = {"Apples", "Foods", "Games", "Sports", "Video Games"};
+		
+		Collections.sort(actual, new SortCategoriesByName());
+		
+		boolean namesMatch = true;
+		
+		for (int i = 0 ; i < actual.size() ; i++) {
+			
+			if (!actual.get(i).getName().equals(expected[i])) {
+				namesMatch = false;
+				break;
+			}
+		}
+		
+		assertEquals(false, namesMatch);
+		
+	}
+	
 	
 	@Test
-	void testSortGroupsByName() {
+	void testSortGroupsByName_Success() {
 		
 		Group g1 = new Group("Sports");
 		Group g2 = new Group("Games");
@@ -81,9 +116,44 @@ class ComparatorTest {
 		
 	}
 	
+	@Test
+	void testSortGroupsByName_Failure() {
+		
+		Group g1 = new Group("Sports");
+		Group g2 = new Group("Games");
+		Group g3 = new Group("Video Games");
+		Group g4 = new Group("Foods");
+		Group g5 = new Group("fail");
+		
+		ArrayList<Group> actual = new ArrayList<Group>();
+		
+		actual.add(g1);
+		actual.add(g2);
+		actual.add(g3);
+		actual.add(g4);
+		actual.add(g5);
+		
+		String expected[] = {"Apples", "Foods", "Games", "Sports", "Video Games"};
+		
+		Collections.sort(actual, new SortGroupsByName());
+		
+		boolean namesMatch = true;
+		
+		for (int i = 0 ; i < actual.size() ; i++) {
+			
+			if (!actual.get(i).getGroupName().equals(expected[i])) {
+				namesMatch = false;
+				break;
+			}
+		}
+		
+		assertEquals(false, namesMatch);
+		
+	}
+	
 	
 	@Test
-	void testSortUsersByName() {
+	void testSortUsersByName_Success() {
 		
 		User u1 = new User("Jack", "jackster3", "HKb@wser!", "10/10/1997", "Valdosta", "Georgia");
 		User u2 = new User("Dan", "theWiz", "WartH@g77", "10/10/1997", "Valdosta", "Georgia");
@@ -117,9 +187,44 @@ class ComparatorTest {
 		
 	}
 	
+	@Test
+	void testSortUsersByName_Failure() {
+		
+		User u1 = new User("Jack", "jackster3", "HKb@wser!", "10/10/1997", "Valdosta", "Georgia");
+		User u2 = new User("Dan", "theWiz", "WartH@g77", "10/10/1997", "Valdosta", "Georgia");
+		User u3 = new User("Carol", "WestCarolina", "P!zzaH$t", "10/10/1997", "Valdosta", "Georgia");
+		User u4 = new User("Dulaney", "LegalTrouble", "D@uble&Tr@uble", "10/10/1997", "Valdosta", "Georgia");
+		User u5 = new User("Ethan", "Fail", "WHY#5", "10/10/1997", "Valdosta", "Georgia");
+		
+		ArrayList<User> actual = new ArrayList<User>();
+		
+		actual.add(u1);
+		actual.add(u2);
+		actual.add(u3);
+		actual.add(u4);
+		actual.add(u5);
+		
+		String expected[] = {"WestCarolina", "theWiz", "LegalTrouble", "IDK", "jackster3"};
+		
+		Collections.sort(actual, new SortUsersByName());
+		
+		boolean namesMatch = true;
+		
+		for (int i = 0; i < actual.size(); i++) {
+			
+			if (!actual.get(i).getId().equals(expected[i])) {
+				namesMatch = false;
+				break;
+			}
+		}
+		
+		assertEquals(false, namesMatch);
+		
+	}
+	
 	
 	@Test
-	void testSortUsersByUsername() {
+	void testSortUsersByUsername_Success() {
 		
 		User u1 = new User("Jack", "jackster3", "HKb@wser!", "10/10/1997", "Valdosta", "Georgia");
 		User u2 = new User("Dan", "theWiz", "WartH@g77", "10/10/1997", "Valdosta", "Georgia");
@@ -150,6 +255,41 @@ class ComparatorTest {
 		}
 		
 		assertEquals(true, namesMatch);
+		
+	}
+	
+	@Test
+	void testSortUsersByUsername_Failure() {
+		
+		User u1 = new User("Jack", "jackster3", "HKb@wser!", "10/10/1997", "Valdosta", "Georgia");
+		User u2 = new User("Dan", "theWiz", "WartH@g77", "10/10/1997", "Valdosta", "Georgia");
+		User u3 = new User("Carol", "WestCarolina", "P!zzaH$t", "10/10/1997", "Valdosta", "Georgia");
+		User u4 = new User("Dulaney", "LegalTrouble", "D@uble&Tr@uble", "10/10/1997", "Valdosta", "Georgia");
+		User u5 = new User("Ethan", "fail", "WHY#5", "10/10/1997", "Valdosta", "Georgia");
+		
+		ArrayList<User> actual = new ArrayList<User>();
+		
+		actual.add(u1);
+		actual.add(u2);
+		actual.add(u3);
+		actual.add(u4);
+		actual.add(u5);
+		
+		String expected[] = {"IDK", "jackster3", "LegalTrouble", "theWiz", "WestCarolina"};
+		
+		Collections.sort(actual, new SortUsersByUsername());
+		
+		boolean namesMatch = true;
+		
+		for (int i = 0; i < actual.size(); i++) {
+			
+			if (!actual.get(i).getId().equals(expected[i])) {
+				namesMatch = false;
+				break;
+			}
+		}
+		
+		assertEquals(false, namesMatch);
 		
 	}
 	
